8339bf5d35cec757a1014b02537aff9caaf9749b
Akande Abdulquadir <akandeabdulquadirolamilekan@gmail.com>
2025-07-29 04:17:35 -0700

    Add CartContext.jsx to version control

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CartProvider } from './context/CartContext.jsx'
+import { CartContext } from './context/CartContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
