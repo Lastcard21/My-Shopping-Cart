98954f53c55b9595332befb25e050f7d3a0e65cc
Akande Abdulquadir <akandeabdulquadirolamilekan@gmail.com>
2025-07-31 10:34:48 -0700

    Fix: added CartContext.jsx and updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { CartProvider } from './context/CartContext.jsx'
+import { CartProvider } from '/src/context/CartContext.jsx'
+
 import './index.css'
 import App from './App.jsx'
 
