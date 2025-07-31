0af75dcbc415aee144a5259c508691c8933a8d33
Akande Abdulquadir <akandeabdulquadirolamilekan@gmail.com>
2025-07-31 10:21:20 -0700

    remove import from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CartProvider } from './context/CartContext.jsx'
-import { CartContext } from './context/CartContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
